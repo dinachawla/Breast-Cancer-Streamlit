cb56e0f52ae5d1b4e6d6cb2b977ef35f50e1cff4
Dina <81174625+dinachawla@users.noreply.github.com>
2025-06-13 15:59:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import joblib
 from pathlib import Path
 
-MODEL_PATH = Path("breast_cancer_clf.pkl")   #changed
+MODEL_PATH = Path("breast_cancer_pipe.pkl")   #changed
 TEST_ACC   = 0.971
 
 @st.cache_resource
